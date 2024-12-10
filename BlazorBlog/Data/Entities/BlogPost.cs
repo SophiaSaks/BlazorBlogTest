3a8b75561896b67b6e852d453048c086f515531a
Sophia S <sophia.saks@live.se>
2024-12-10 16:00:54 +0100

    initial commit

---
+++ BlazorBlog/Data/Entities/BlogPost.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq.Expressions;
+
+namespace BlazorBlog.Data.Entities
+{
+    public class BlogPost
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Content { get; set; }
+        public string Author { get; set; } = string.Empty;
+        public DateTime Created { get; set; } = DateTime.Now;
+        public bool IsPublished { get; set; } = true;
+        public bool IsDeleted { get; set; } = false;
+        public string Url { get; set; } = string.Empty;
+    }
+
+  
+}
